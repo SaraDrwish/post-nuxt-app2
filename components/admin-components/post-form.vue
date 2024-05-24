7705e35de9b13ff4c89a55c89d6e98e00f23c17c
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-05-24 17:09:44 +0300

    n v 5

---
+++ components/admin-components/post-form.vue
@@ -0,0 +1,91 @@
+<template>
+
+  <v-form v-model="valid">
+    <h2>post-form-component</h2>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="firstname"
+            :counter="10"
+            :rules="nameRules"
+            label="First name"
+            hide-details
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="lastname"
+            :counter="10"
+            :rules="nameRules"
+            label="Last name"
+            hide-details
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            hide-details
+            required
+          ></v-text-field>
+          <v-textarea
+            label="Label"
+            model-value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+            name="input-7-1"
+            variant="filled"
+            auto-grow
+        ></v-textarea>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+<script>
+  export default {
+    data: () => ({
+      valid: false,
+      firstname: '',
+      lastname: '',
+      nameRules: [
+        value => {
+          if (value) return true
+
+          return 'Name is required.'
+        },
+        value => {
+          if (value?.length <= 10) return true
+
+          return 'Name must be less than 10 characters.'
+        },
+      ],
+      email: '',
+      emailRules: [
+        value => {
+          if (value) return true
+
+          return 'E-mail is requred.'
+        },
+        value => {
+          if (/.+@.+\..+/.test(value)) return true
+
+          return 'E-mail must be valid.'
+        },
+      ],
+    }),
+  }
+</script>
---
+++ pages/admin/post/add.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>add post page</h2>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name:"addPo"
+  }
+</script>
